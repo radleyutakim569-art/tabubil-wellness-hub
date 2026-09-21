2e3f98ce8fbbb8663092163698fce23c4e255d42
radleyutakim569-art <radleyutakim569@gmail.com>
2026-09-22 00:35:31 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Tabubil Wellness Hub JavaScript */
+
+console.log("Welcome to Tabubil Wellness Hub!");
+
+/* Automatically update the copyright year */
+
+const year = document.getElementById("year");
+
+if (year) {
+    year.textContent = new Date().getFullYear();
+}
